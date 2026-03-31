4fe9d7d6cb363ab7840f1dc8ec8454448ea81edc
R H <165597973+rhuda21@users.noreply.github.com>
2026-03-31 10:12:34 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 title Get Token - Setup
-pip install pywebview[qt] curl-cffi pycryptodome pywin32 aiohttp pystyle
+pip install pywebview[qt] curl-cffi pycryptodome pywin32 aiohttp pystyle colorama
 echo Installed Dependencies
 PAUSE
